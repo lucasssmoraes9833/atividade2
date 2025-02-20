c211c7b6a14852b4aad98603d2296251384a5db4
lucasssmoraes9833 <165032734+lucasssmoraes9833@users.noreply.github.com>
2025-02-20 10:49:15 -0300

    Add files via upload

---
+++ inverter.js
@@ -0,0 +1,4 @@
+let str = "Javascript"
+let invertida = str.split("").reverse().join("")
+
+console.log (invertida)
---
+++ maiornumero.js
@@ -0,0 +1,4 @@
+var array = [8,9,3,7];
+var maior = Math.max(...array);
+
+console.log ("O Maior valor é: " ,maior);
